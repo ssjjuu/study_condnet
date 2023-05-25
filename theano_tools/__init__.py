8fbf8a536c4afc8dd2eebca2fd5aa2ae6bb6d270
dongjaekim-hail <dongjaekim@dankook.ac.kr>
2023-05-25 20:25:16 +0900

    update

--- theano_tools/__init__.py
+++ theano_tools/__init__.py
@@ -235,7 +235,7 @@ class GenericClassificationDataset:
 
     def validMinibatches(self, minibatch_size=32):
         nminibatches = self.valid[0].shape[0] / minibatch_size
-        indexes = numpy.arange(nminibatches)
+        indexes = numpy.arange(nminibatches).astype('int32')
         numpy.random.shuffle(indexes)
         for i in indexes:
             yield self.pp(self.valid[0][i*minibatch_size:(i+1)*minibatch_size],
@@ -243,7 +243,7 @@ class GenericClassificationDataset:
 
     def testMinibatches(self, minibatch_size=32):
         nminibatches = self.test[0].shape[0] / minibatch_size
-        indexes = numpy.arange(nminibatches)
+        indexes = numpy.arange(nminibatches).astype('int32')s
         numpy.random.shuffle(indexes)
         for i in indexes:
             yield self.pp(self.test[0][i*minibatch_size:(i+1)*minibatch_size],
